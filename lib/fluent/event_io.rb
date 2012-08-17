65696770c39884618c26a27b4aa540c46625e291
mzp <mzpppp@gmail.com>
2012-08-17 23:43:51 +0900

    fix segfalut on callback

--- lib/fluent/event_io.rb
+++ lib/fluent/event_io.rb
@@ -19,7 +19,32 @@
 module Fluent
 module EventIO
 
+module Listener
+  private
+  def callbacks
+    @callbacks ||= Set.new
+  end
+
+  def callback(&f)
+    const_name = "Callback_#{f.object_id}"
+    unless self.class.const_defined?(const_name)
+      callbacks << const_name
+      self.class.const_set(const_name, f)
+    end
+    self.class.const_get(const_name)
+  end
+
+  def clear_callbacks
+    callbacks.each do |name|
+      self.class.send(:remove_const, name)
+    end
+    callbacks.clear
+  end
+end
+
 class Loop
+  include Listener
+
   class << self
     def default
       self.new Foolio::UV.default_loop
@@ -33,51 +58,52 @@ class Loop
   def initialize(loop)
     @loop = loop
 
-    idle = self.idle {|_|
+    idle do
       if @stop
-        $log.info "close all handles"
-        Foolio::UV.close_all @loop
+        $log.info "closing libuv handlers"
+        Foolio::UV.close_all(@loop, callback{
+                               clear_callbacks
+                             })
       end
-    }
+    end
   end
 
   def idle(&block)
     handle = Foolio::UV.idle_init(@loop)
-    Foolio::UV.idle_start(handle,
-                          lambda {|_| block[] })
+    Foolio::UV.idle_start(handle, callback{|_| block[] })
   end
 
   def timer(interval, repeat, &block)
     handle = Foolio::UV.timer_init(@loop)
     sec    = (interval * 1000).to_i
     Foolio::UV.timer_start(handle,
-                           proc {|_| block[] },
+                           callback{|_| block[] },
                            sec,
                            sec)
   end
 
   def tcp(ip, port)
-    handle = Foolio:UV.tcp_init(@loop)
-    Foolio::UV.tcp_bind(handle, Foolio::UV.ip4_addr(ip, port))
-    Fluent::EventIO::TCP.new @loop, handle
+     handle = Foolio::UV.tcp_init(@loop)
+     Foolio::UV.tcp_bind(handle, Foolio::UV.ip4_addr(ip, port))
+     Fluent::EventIO::TCP.new @loop, handle
   end
 
   def udp
-    handle = Foolio:UV.udp_init(@loop)
+    handle = Foolio::UV.udp_init(@loop)
     Fluent::EventIO::UDP.new @loop, handle
   end
 
-  def unix(path)
-    pipe = @loop.pipe.bind(path)
-    at_close pipe
-    Fluent::EventIO::TCP.new self, pipe
-  end
+  # def unix(path)
+  #   pipe = @loop.pipe.bind(path)
+  #   at_close pipe
+  #   Fluent::EventIO::TCP.new self, pipe
+  # end
 
-  def file_stat(path, &block)
-    fs = @loop.fs_event(path, &block)
-    at_close fs
-    fs
-  end
+  # def file_stat(path, &block)
+  #   fs = @loop.fs_event(path, &block)
+  #   at_close fs
+  #   fs
+  # end
 
   def run
     Foolio::UV.run @loop
@@ -98,20 +124,20 @@ class Loop
 end
 
 class Handler
+  include Listener
   def initialize(io)
     @io = io
   end
 
   def write(data)
-    callback = proc do|req, status|
-      on_write_complete()
-    end
-    @io.write(data, callback)
-    Foolio::UV.write(nil, @io, data, callback)
+    Foolio::UV.write(nil, @io, data, callback do|status|
+                       on_write_complete(status)
+                     end)
   end
 
   def close
-    Foolio::UV.close(@io, proc{})
+    $log.info "close #{@io}"
+    Foolio::UV.close(@io, callback{})
   end
 
   def on_write_complete; end
@@ -141,18 +167,22 @@ class TCP
 
   def listen_handler(backlog, klass, *args)
     callback = proc do|status|
-      client = Foolio:UV.tcp_init(@loop)
-      @stream.accept(@server, client)
+      client = Foolio::UV.tcp_init(@loop)
+      Foolio::UV.accept(@stream, client)
       $log.info "accept #{client}"
       handle = klass.new(client, *args)
       handle.on_connect
 
       on_read = proc {|data|
-        handle.on_read(data)
+        if data then
+          handle.on_read(data)
+        else
+          handle.close
+        end
       }
       Foolio::UV.read_start(client, on_read)
     end
-    FoolIO::UV.listen(@stream, backlog, callback)
+    Foolio::UV.listen(@stream, backlog, callback)
     self
   end
 
@@ -162,21 +192,22 @@ class TCP
 end
 
 class UDP
+  include Listener
   def initialize(loop, udp)
     @loop = loop
     @udp = udp
   end
 
   def bind(ip,port)
-    Foolio::UV.udp_bind(handle, Foolio::UV.ip4_addr(ip, port), 0)
+    Foolio::UV.udp_bind(@udp, Foolio::UV.ip4_addr(ip, port), 0)
     self
   end
 
   def start(&block)
-    on_recv = proc do|data, addr, flags|
+    on_recv = callback do|data, addr, flags|
       ip = Foolio::UV.ip_name addr
       port = Foolio::UV.port addr
-      block.call(ip, port, data)
+      block.call(ip, port, data) if ip
     end
     Foolio::UV.udp_recv_start(@udp, on_recv)
   end
